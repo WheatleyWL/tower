b165a5e97f716a18ad4e54225f3656e0d230868c
Mikhail <m.uriev@notamedia.com.ru>
2023-04-04 17:22:20 +0300

    replaced from packacke into project some files,test

--- src/Traits/Attachable.php
+++ src/Traits/Attachable.php
@@ -3,11 +3,12 @@
 namespace zedsh\tower\Traits;
 
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use App\Models\File;
 
 trait Attachable
 {
     public function files()
     {
-        return $this->morphToMany(zedsh\tower\Models\File::class, 'attachable');
+        return $this->morphToMany(File::class, 'attachable');
     }
 }
--- src/towerServiceProvider.php
+++ src/towerServiceProvider.php
@@ -26,7 +26,7 @@ class towerServiceProvider extends ServiceProvider
         }
 
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'tower');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+//        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 //        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 
